82e9cac1d7028aca0df090069ddf0cbab9e354bf
hanets <han3ts@gmail.com>
2026-02-06 13:25:48 +0700

    soal

---
+++ Soal_Indra/soal1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct dataMahasiswa
+{
+    string nama;
+    int umur;
+    float ipk;
+};
+
+int main()
+{
+    dataMahasiswa mhs;
+    cout << "masukan nama: ";
+    cin >> ;
+}
